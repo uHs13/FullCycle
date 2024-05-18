ba82acbb8670e42e765e3e6068cb1b3cfc07a31a
heitorsouza <heitorhenriquedias@gmail.com>
2024-05-17 23:38:14 -0300

    feature: starting to create some automated tests to validate the taticsNpatterns entities and value objects

---
+++ ddd/taticsNpatterns/src/entity/customer.spec.ts
@@ -0,0 +1,8 @@
+describe('Customer unit tests', () => {
+    it('should sum and return the correct result', () => {
+        let numberOne = 10;
+        let numberTwo = 20;
+
+        expect(numberOne + numberTwo).toBe(30);
+    });
+});
