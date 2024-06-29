d1cf8a8ee4055f4154d18665048ed86938214289
heitorsouza <heitorhenriquedias@gmail.com>
2024-06-29 15:01:45 -0300

    feature: configuring sequelize and creating product model inside the ddd tatics and patterns

--- ddd/taticsNpatterns/src/main.ts
+++ ddd/taticsNpatterns/src/main.ts
@@ -1,8 +1,8 @@
-import Address from "./entity/address";
-import Customer from "./entity/customer";
-import Order from "./entity/order";
-import OrderItem from "./entity/orderItem";
-import Product from "./entity/product";
+import Address from "./domain/entity/address";
+import Customer from "./domain/entity/customer";
+import Order from "./domain/entity/order";
+import OrderItem from "./domain/entity/orderItem";
+import Product from "./domain/entity/product";
 
 const address = new Address('Groove Street', 485, '123456', 'LS');
 
