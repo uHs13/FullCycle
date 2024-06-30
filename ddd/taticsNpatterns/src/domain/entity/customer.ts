3933b308129ad3395c62bc6295ec9eee351f0e09
heitorsouza <heitorhenriquedias@gmail.com>
2024-06-30 20:47:21 -0300

    feature: creating customer repository and customer model with tdd tatic

--- ddd/taticsNpatterns/src/domain/entity/customer.ts
+++ ddd/taticsNpatterns/src/domain/entity/customer.ts
@@ -3,8 +3,8 @@ import Address from "./address";
 export default class Customer {
     private _id: string;
     private _name: string;
-    private _address!: Address;
     private _active: boolean = true;
+    private _address!: Address;
 
     constructor(id: string, name: string) {
         this._id = id;
@@ -17,12 +17,20 @@ export default class Customer {
         this._address = address;
     }
 
+    set active(active: boolean) {
+        this._active = active;
+    }
+
     get id(): string {
         return this._id;
     }
 
     get name(): string {
         return this._name;
+    }
+
+    get address(): Address {
+        return this._address;
     }
 
     validate() {
---
+++ ddd/taticsNpatterns/src/domain/repository/customerRepositoryInterface.ts
@@ -0,0 +1,6 @@
+import Customer from "../entity/customer";
+import RepositoryInterface from "./repositoryInterface";
+
+export default interface CustomerRepositoryInterface extends RepositoryInterface<Customer> {
+
+}
